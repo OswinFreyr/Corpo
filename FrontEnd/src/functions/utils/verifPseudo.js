58866417e79493b826a123f1032a2e9bf3192e6e
TanguyBoonty <pisanotanguy@gmail.com>
2025-02-26 22:59:04 +0100

    debut modif verification pseudo

--- FrontEnd/src/functions/utils/verifPseudo.js
+++ FrontEnd/src/functions/utils/verifPseudo.js
@@ -1,16 +1,24 @@
+export default function verifPseudo(pseudo) {
+  const bannedWords = [
+      "admin", "moderator", "root", "boss", "staff", "support", "owner",
+      "con", "merde", "putain", "fdp", "salope", "connard", "abruti", "idiot",
+      "fuck", "shit", "bitch", "asshole", "bastard", "dumbass", "moron", "cunt",
+      "mierda", "puta", "gilipollas", "cabron", "estupido", "pendejo", "coño",
+      "hitler", "nazi", "racist", "slave", "terrorist", "killer", "murder",
+      "hacker", "cheater", "sex", "porn", "drug", "scam", "fraud", "mod",
+      "administ", "sysadmin", "god", "dev", "test", "bot", " ", ""
+  ];
 
-export default function verifPseudo (pseudo) {
-    const bannedWords = [
-        "admin", "moderator", "root", "boss", "badword",
-        "con", "merde", "putain", "fdp", "salope", "connard", "abruti", "idiot", 
-        "fuck", "shit", "bitch", "asshole", "bastard", "dumbass", "moron", 
-        "mierda", "puta", "gilipollas", "cabron", "estupido","hitler", " "
-      ];
+  const normalizeText = (text) => {
+      return text.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase();
+  };
 
-      const normalizeText = (text) => {
-        return text.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase();
-    };
+  const normalizedPseudo = normalizeText(pseudo);
 
-    return bannedWords.some(word => normalizeText(pseudo).includes(word));
-
+  // Check length && bad words
+  if ((normalizedPseudo.length > 3 && normalizedPseudo.length < 11) && !(bannedWords.some(word => normalizedPseudo.includes(word)))) {
+    return false;
+  } else {
+    return true;
+  }
 }
