4845e06ef6058fa665352b81d80e56acbe6ed748
Téo Ladouche <teo.ladouche@swan.io>
2025-02-28 09:56:12 +0100

    new answer layout

--- FrontEnd/src/components/Answer.vue
+++ FrontEnd/src/components/Answer.vue
@@ -1,17 +1,26 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import "xp.css/dist/XP.css";
 
 const props = defineProps<{
   answer: { answer: string; productivity: number; wellbeing: number; treasury: number; environment: number };
+  questionNumber: string
 }>();
 
 const emit = defineEmits(['selectedAnswer']);
+
+const buttonRef = ref(null);
+
+defineExpose({
+  buttonRef
+});
+
 </script>
 
 <template>
   <div class="window" style="width: 400px">
     <div class="title-bar">
-      <div class="title-bar-text">Réponse choisie</div>
+      <div class="title-bar-text">Réponse {{ props.questionNumber }}</div>
       <div class="title-bar-controls">
         <button aria-label="Minimize"></button>
         <button aria-label="Maximize"></button>
@@ -21,7 +30,7 @@ const emit = defineEmits(['selectedAnswer']);
 
     <div class="window-body">
       <p class="answer-text">{{ props.answer.answer }}</p>
-      <button @click="emit('selectedAnswer', props.answer)">Répondre</button>
+      <button @click="emit('selectedAnswer', props.answer)" ref="buttonRef">Répondre</button>
     </div>
   </div>
 </template>
